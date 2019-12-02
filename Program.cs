a37cba17675b41403e04781a8dd6f460376373a5
unknown <papaaced@gmail.com>
2019-12-01 23:57:14 -0500

    Improved day 1 solution

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 
 namespace AdventOfCode
 {
-    class Program
+    class ClipBoard
     {
         [DllImport("user32.dll")]
         internal static extern bool OpenClipboard(IntPtr hWndNewOwner);
@@ -15,25 +15,52 @@ namespace AdventOfCode
 
         [DllImport("user32.dll")]
         internal static extern bool SetClipboardData(uint uFormat, IntPtr data);
+
+        public static void Set(string s)
+        {
+            OpenClipboard(IntPtr.Zero);
+            var ptr = Marshal.StringToHGlobalUni(s);
+            SetClipboardData(13, ptr);
+            CloseClipboard();
+            Marshal.FreeHGlobal(ptr);
+        }
+    }
+
+    //class Day1
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        var allLines = File.ReadAllLines(@"C:\Users\battlepants\OneDrive\AdventOfCode\input.txt");
+
+    //        var sol = allLines.Select(int.Parse)
+    //             .Aggregate(0, (sum, current) => sum += SumR(current) - current);
+
+
+    //        var result = sol;
+            
+    //        ClipBoard.Set(result.ToString());
+    //        System.Console.WriteLine(result);
+    //        Console.WriteLine();
+
+    //        int SumR(int init)
+    //        {
+    //            if (init <= 0)
+    //                return 0;
+    //            return init + SumR(init / 3 - 2);
+    //        }
+    //    }
+    //}
+
+    class Day2
+    {
         static void Main(string[] args)
         {
             var allLines = File.ReadAllLines(@"C:\Users\battlepants\OneDrive\AdventOfCode\input.txt");
 
-           var sol = allLines.Select(int.Parse)
-                .Select(x => SumR(x) - x)
-                .Sum();
-
-
-            
             var result = sol;
-
-            OpenClipboard(IntPtr.Zero);
-            var ptr = Marshal.StringToHGlobalUni(result.ToString());
-            SetClipboardData(13, ptr);
-            CloseClipboard();
-            Marshal.FreeHGlobal(ptr);
-
-            System.Console.WriteLine(result);
+            
+            ClipBoard.Set(result);
+            Console.WriteLine(result);
             Console.WriteLine();
 
             int SumR(int init)
